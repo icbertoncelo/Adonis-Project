e074c1b877db27b32e8e86436f41598454881c37
Ian Carlos <carlos.ian007@gmail.com>
2019-05-21 20:37:22 -0300

    Feat(mail): Set up email sending

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,6 +14,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('carlos.ian007@gmail.com', 'Ian | UENF')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (err) {
       response.status(err.status).send({
         error: { message: 'Something is wrong. Does this email exist?' }
