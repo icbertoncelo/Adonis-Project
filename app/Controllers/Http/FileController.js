dc5fca4926484aed258fad75908eef364278da2e
Ian Carlos <carlos.ian007@gmail.com>
2019-05-21 21:48:54 -0300

    Feat(upload): Add file upload method

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const File = use('App/Models/File')
+const Helpers = use('Helpers')
+
+class FileController {
+  async store ({ request, response }) {
+    try {
+      if (!request.file('file')) return
+
+      const upload = request.file('file', { size: '2mb' })
+      const fileName = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(Helpers.tmpPath('uploads'), { name: fileName })
+
+      if (!upload.move()) {
+        throw upload.error()
+      }
+
+      const file = await File.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      })
+
+      return file
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ err: { message: 'File upload error' } })
+    }
+  }
+}
+
+module.exports = FileController
