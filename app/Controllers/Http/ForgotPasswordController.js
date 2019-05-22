3346a745fa2bb0fec5ff645115ef1dcc290b31bd
Ian Carlos <carlos.ian007@gmail.com>
2019-05-21 21:04:55 -0300

    Feat(pass): Add update password method

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -30,8 +31,36 @@ class ForgotPasswordController {
         }
       )
     } catch (err) {
-      response.status(err.status).send({
+      return response.status(err.status).send({
         error: { message: 'Something is wrong. Does this email exist?' }
+      })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'The token was expired' }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: 'Something is wrong' }
       })
     }
   }
