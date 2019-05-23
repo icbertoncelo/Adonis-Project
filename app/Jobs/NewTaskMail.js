6c8e64a4081245a6cfd9abe80da1a4ed05430ab5
Ian Carlos <carlos.ian007@gmail.com>
2019-05-23 20:39:42 -0300

    Feat(queue): Configure Redis

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,40 @@
+'use strict'
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ email, username, title, file }) {
+    console.log(`Job: ${NewTaskMail.key}`)
+
+    await Mail.send(
+      ['emails.new_task'],
+      {
+        username,
+        title,
+        hasAttachment: !!file
+      },
+      message => {
+        message
+          .to(email)
+          .from('carlos.ian007@gmail.com', 'Ian | UENF')
+          .subject('New task for you')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          })
+        }
+      }
+    )
+  }
+}
+
+module.exports = NewTaskMail
